ef6310f7d8808a7083bde21828265f774d48003f
Dan Meliza <dan@meliza.org>
2015-05-14 17:04:49 -0400

    communicating with controller

--- project.clj
+++ project.clj
@@ -8,9 +8,12 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2173"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/tools.logging "0.3.1"]
 
+                 [com.novemberain/monger "2.0.0"]
                  [com.keminglabs/zmq-async "0.1.0"]
-                 [cheshire "5.4.0"]]
+                 [cheshire "5.4.0"]
+                 [clj-time "0.8.0"]]
 
   :main ^:skip-aot decide-host.core
   :target-path "target/%s"
--- src/decide_host/core.clj
+++ src/decide_host/core.clj
@@ -3,52 +3,90 @@
   (:require [com.keminglabs.zmq-async.core :refer [register-socket!]]
             [clojure.core.async :as async :refer [>! <! >!! <!!]]
             [clojure.core.match :refer [match]]
+            [clj-time.core :as t]
             [cheshire.core :as json]))
 
-(def config (json/parse-string (slurp "config/host-config.json")))
+(def config (json/parse-string (slurp "config/host-config.json") true))
+
+(def zmq-in (async/chan (async/sliding-buffer 64)))
+(def zmq-out (async/chan (async/sliding-buffer 64)))
 
 (defn to-string
   "safely converts a byte array or nil to a string; nil => ''"
-  [x] (apply str (map char x)))
+  [x]
+  (when-not (nil? x) (String. x)))
 
-(defn decode-data
+(defn decode-pub
   [bytes]
   (json/parse-string (to-string bytes)))
 
-(defn zmq-listener
-  "Starts listening for messages on addr. Returns input and output channels"
-  [addr]
-  (let [in (async/chan (async/sliding-buffer 64))
-        out (async/chan (async/sliding-buffer 64))]
-    (register-socket! {:in in :out out :socket-type :router
-                       :configurator (fn [socket] (.bind socket addr))})
-    [in out]))
+(def controllers (atom {}))
 
-(defn zmq-handler
-  [[in out]]
-  (async/go-loop [[id & msg] (<! out)]
+(defn its-alive!
+  "Updates the liveness status for client id"
+  [id]
+  (swap! controllers assoc-in [id :last-seen] (t/now)))
+
+(defn connect!
+  "Registers a controller as connected. Returns true if successful, nil if the address is taken"
+  [id ctrl-addr]
+  (swap! controllers assoc id {:addr ctrl-addr :last-seen (t/now) :alive true})
+  true)
+
+(defn disconnect!
+  "Unregisters a controller"
+  [id]
+  (swap! controllers dissoc id))
+
+(defn check-connection
+  [id]
+  (let [last (get-in @controllers [id :last-seen])
+        hb (config :heartbeat-ms)
+        hb-max (* hb (config :heartbeat-liveness))
+        interval (t/in-millis (t/interval last (t/now)))]
+    (cond
+     (> interval hb) (async/put! zmq-in [id "HUGZ"]))))
+
+(defn bad-message
+  "Handle unrecognized message types"
+  [id mtype]
+  ;; nil indicates the server is shutting down
+ (when-not nil?
+   (println "W: bad message:" id mtype)))
+
+;;; the zmq message handling loop
+(async/go-loop [[id & msg] (<! zmq-out)]
+  (when-let [id (String. id)]
     (case (to-string (first msg))
-      "OHAI"
-      (let [client-name (to-string (second msg))]
-        (println "OHAI" client-name)
-        (>! in [id "OHAI-OK"]))
-      "PUB"
-      (let [data (decode-data (second msg))]
-        (println "PUB" data)
-        (>! in [id "ACK" "insert-hash-here"]))
-      "HUGZ"
-      (do
-        (println "HUGZ from" (to-string id))
-        (>! in [id "HUGZ-OK"]))
-      "BYE"
-      (println "BYE from" (to-string) id))
-    (recur (<! out))))
+      "OHAI" (let [ctrl-addr (to-string (second msg))]
+               (println "D:" id "OHAI" ctrl-addr)
+               (let [out-msg (if (connect! id ctrl-addr) "OHAI-OK" "WTF")]
+                 (>! zmq-in [id out-msg])))
+      "PUB" (let [data (decode-pub (second msg))]
+              (println "PUB" data)
+              (>! zmq-in [id "ACK" "insert-hash-here"]))
+      "HUGZ" (do
+               (its-alive! id)
+               (>! zmq-in [id "HUGZ-OK"]))
+      "HUGZ-OK" (its-alive! id)
+      "BYE" (do
+              (println "D:" id "BYE")
+              (disconnect! id))
+      (bad-message msg)))
+  (recur (<! zmq-out)))
+
+#_(async/go-loop []
+  (<! (async/timeout (config :heartbeat-ms)))
+  (dorun (map check-connection (keys @controllers))))
+
+(defn start-zmq-server [addr]
+  (register-socket! {:in zmq-in :out zmq-out :socket-type :router
+                       :configurator (fn [socket] (.bind socket addr))}))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-
-  )
+)
 
 (comment
   (require '[clojure.pprint :refer [pprint]]
