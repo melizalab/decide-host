0cb630f31f9e87a8ccd979867562b47071839003
Dan Meliza <dan@meliza.org>
2015-05-26 14:48:50 -0400

    got frodo working, more or less

--- src/decide_host/handlers.clj
+++ src/decide_host/handlers.clj
@@ -8,38 +8,40 @@
 
 (defn add-handler
   "Adds an asynchronous handler f to pub for one or more topics"
-  [pub f & topics]
-  (when-let [tops (seq topics)]
-    (let [chan (async/chan)]
-      (dorun (map #(async/sub pub % chan) topics))
-      (async/go-loop [data (<! chan)]
-        (when data
-          (f data)
-          (recur (<! chan)))))))
+  [context f & topics]
+  (let [{{pub :event-pub} :msg-handler} context]
+    (when-let [tops (seq topics)]
+      (let [chan (async/chan)]
+        (dorun (map #(async/sub pub % chan) topics))
+        (async/go-loop [data (<! chan)]
+          (when data
+            (f context data)
+            (recur (<! chan))))))))
 
 (defn update-subject!
   "Updates subject documents with event data"
-  [db data]
+  [context data]
   #_(println "D: update-subject!" data)
-  (match [data]
+  (let [{{db :db} :database} context]
+    (match [data]
 
-         [{:topic :state-changed :name "experiment"
-           :subject s :procedure p :user u :time t :addr a}]
-         (if-not (nil? s)
+           [{:topic :state-changed :name "experiment"
+             :subject s :procedure p :user u :time t :addr a}]
+           (if-not (nil? s)
              (db/start-subject! db s {:procedure p :controller a :user u :start-time t})
              (db/stop-subject! db a t))
 
-         [{:topic :state-changed :name "hopper" :up state :addr a :time t}]
-         (when state
-           (db/update-subject-by-controller! db a {:last-fed t}))
+           [{:topic :state-changed :name "hopper" :up state :addr a :time t}]
+           (when state
+             (db/update-subject-by-controller! db a {:last-fed t}))
 
-         [{:topic :state-changed :addr a :time t}]
-         (db/update-subject-by-controller! db a {:last-event t})
+           [{:topic :state-changed :addr a :time t}]
+           (db/update-subject-by-controller! db a {:last-event t})
 
-         [{:topic :trial-data :subject s :trial n :time t}]
-         (db/update-subject! db s {:last-trial t})
+           [{:topic :trial-data :subject s :trial n :time t}]
+           (db/update-subject! db s {:last-trial t})
 
-         [{:topic :trial-data :subject s :experiment e}]
-         (db/update-subject! db s {:experiment e})
+           [{:topic :trial-data :subject s :experiment e}]
+           (db/update-subject! db s {:experiment e})
 
-         :else nil))
+           :else nil)))
--- src/decide_host/web.clj
+++ src/decide_host/web.clj
@@ -5,18 +5,23 @@
             [ring.middleware.format :refer [wrap-restful-format]]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
             [compojure.core :refer [routes context GET]]
-            [compojure.route :refer [resources]]))
+            [compojure.route :refer [resources]]
+            [decide-host.config :refer [config]]
+            [decide-host.host :as host]
+            [decide-host.handlers :refer [add-handler update-subject!]]))
 
 (defn site-routes []
   (routes
     (GET "/" [] "hello world")
     (resources "/")))
 
-#_(defn -main [& args]
-  (run-server (wrap-defaults (site-routes) api-defaults) {:port 8080}))
-
 (def app
   (reify App
     (start! [_]
-      {:frodo/handler (wrap-defaults (site-routes) api-defaults)})
-    (stop! [_ system])))
+      (let [{{db-uri :uri} :database {zmq-addr :endpoint} :host} (config)
+            context (host/start! db-uri zmq-addr)]
+        (add-handler context update-subject! :state-changed :trial-data)
+        {:context context
+         :frodo/handler (wrap-defaults (site-routes) api-defaults)}))
+    (stop! [_ system]
+      (host/stop! (:context system)))))
