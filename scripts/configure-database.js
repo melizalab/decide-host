9654866e6fe37de5a67b1f9c027abdc06337e3e8
Dan Meliza <dan@meliza.org>
2015-06-19 18:24:23 -0400

    remove a database index that can *f* up some queries

--- scripts/configure-database.js
+++ scripts/configure-database.js
@@ -6,7 +6,7 @@ db.events.ensureIndex({addr: 1, name: 1});
 db.events.ensureIndex({addr: 1, time: 1});
 
 db.trials.ensureIndex({addr: 1, name: 1});
-db.trials.ensureIndex({addr: 1, time: 1});
+db.trials.ensureIndex({subject: 1, time: 1});
 db.trials.ensureIndex({subject: 1, experiment: 1, trial: 1})
 
 db.subjects.ensureIndex({addr: 1});
